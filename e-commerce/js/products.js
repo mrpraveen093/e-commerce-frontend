31828175b7e651615b95829ecbf7948bbef839c8
mrpraveen093 <rockypavya33@gmail.com>
2025-05-06 16:42:22 +0530

    Add files via upload

---
+++ e-commerce/js/products.js
@@ -0,0 +1,51 @@
+const products = [
+    {
+        id: 1,
+        title: "Wireless Headphones",
+        price: 1999,
+        description: "Noise Cancelling over-ear Bluetooth Headphones",
+        category: "Electronics",
+        image: "images/headphone.jpg",
+    },
+    {
+        id: 2,
+        title: "Smart Watch",
+        price: 2999,
+        description: "Fitness Smart Watch with Heart Rate Monitor",
+        category: "Electronics",
+        image: "images/smart watch.jpg",
+    },
+    {
+        id: 3,
+        title: "T-shirt",
+        price: 799,
+        description: "100% Cotton Printed T-shirt",
+        category: "Clothing",
+        image: "images/T-shirt.webp",
+    },
+    {
+        id: 4,
+        title: "Mobiles",
+        price: 26499,
+        description: "64 GB RAM  ",
+        category: "Electronics",
+        image: "images/mobiles.webp",
+    },
+    {
+        id: 5,
+        title: "snekers",
+        price: 12499,
+        description: "Comfortable to walk easy to run",
+        category: "Footwear",
+        image: "images/snekers.webp",
+    },
+    {
+        id: 6,
+        title: "Laptop",
+        price: 49999,
+        description: "64 GB RAM and 512 GB SSD Memory Card ",
+        category: "Electronics",
+        image: "images/laptop.jpg",
+    }
+    // Add more product as needed
+];
